fb50d4b2340b58f04fda3d2620f3b9edd55f0288
ytkt <yyttkktt@gmail.com>
2019-05-08 21:45:01 +0900

    Learn about style

--- App.js
+++ App.js
@@ -1,34 +1,25 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isShowingText: true };
-
-    setInterval(() => (
-      this.setState(previousState => (
-        { isShowingText: !previousState.isShowingText }
-      ))
-    ), 1000);
+const styles = StyleSheet.create({
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
   }
-
-  render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-
-    return (
-        <Text>{this.props.text}</Text>
-    );
-  }
-}
+});
 
 export default class App extends React.Component {
   render() {
     return (
         <View style={{alignItems: 'center', top: 50}}>
-        <Blink text='I love to blink' />
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigBlue}>just BigBlue</Text>
+        <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+        <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
         </View>
     );
   }
