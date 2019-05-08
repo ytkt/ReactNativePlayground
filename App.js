7526c3abc77dcac3c097ba01d6a9ba23d4f29d18
ytkt <yyttkktt@gmail.com>
2019-05-08 21:21:14 +0900

    Learn about props

--- App.js
+++ App.js
@@ -1,27 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default class App extends React.Component {
+class Greeting extends Component {
   render() {
-    let pic = {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>hello React!!</Text>
-        <Image source={pic} style={{width: 420, height: 110}}/>
-      </View>
+        <View style={{alignItems: 'center'}}>
+        <Text>Hello {this.props.name}!</Text>
+        </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends React.Component {
+  render() {
+    return (
+        <View style={{alignItems: 'center', top: 50}}>
+        <Greeting name='Hi' />
+        <Greeting name='There' />
+        <Greeting name='What is up' />
+        </View>
+    );
+  }
+}
