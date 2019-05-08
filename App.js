8438a349b9a3eee6447d2d4de0c2ff4403a9107c
ytkt <yyttkktt@gmail.com>
2019-05-08 22:10:18 +0900

    Learn about justify content

--- App.js
+++ App.js
@@ -4,7 +4,16 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-        <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{
+          flex: 1,
+          flexDirection: 'column',
+//          justifyContent: 'space-between',
+//          justifyContent: 'flex-start',
+//          justifyContent: 'center',
+//          justifyContent: 'flex-end',
+//          justifyContent: 'space-around',
+          justifyContent: 'space-evenly',
+        }}>
         <View style={{width: 100, height: 100, backgroundColor: 'powderblue'}} />
         <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
         <View style={{width: 100, height: 100, backgroundColor: 'steelblue'}} />
