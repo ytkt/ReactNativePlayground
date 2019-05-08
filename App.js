2efd1dcb37c105825109cf39385dc43b72aa2d05
ytkt <yyttkktt@gmail.com>
2019-05-09 02:36:18 +0900

    Learn about FlatList

--- App.js
+++ App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import { Alert, ScrollView, Image, Text, View } from 'react-native';
+import { Alert, StyleSheet, FlatList, Image, Text, View } from 'react-native';
 
 export default class App extends Component {
-  _onPressButton() {
-    Alert.alert('you tapped the button!!');
-  }
-
   render() {
     return (
-        <ScrollView horizontal={false} bounces={true}>
-        <Text style={{fontSize: 96}}>Scroll me plz</Text>
-        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-        <Text style={{fontSize: 96}}>end of ScrollView</Text>
-        </ScrollView>
+        <View style={styles.container}>
+  <FlatList
+          data={[
+            {key: 'hoge'},
+            {key: 'fuga'},
+            {key: 'piyo'},
+            {key: 'hogehoge'},
+            {key: 'fugafuga'},
+            {key: 'piyopiyo'},
+            {key: 'hogehogehoge'},
+            {key: 'fugafugafuga'},
+            {key: 'piyopiyopiyo'},
+          ]}
+      renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+        </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 36,
+    height: 88,
+  },
+})
