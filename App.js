13d61f6efdd74fc36d04a257d08685340317e875
ytkt <yyttkktt@gmail.com>
2019-05-08 21:51:17 +0900

    Learn fixed dimensions

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-const styles = StyleSheet.create({
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  }
-});
-
 export default class App extends React.Component {
   render() {
     return (
-        <View style={{alignItems: 'center', top: 50}}>
-        <Text style={styles.red}>just red</Text>
-        <Text style={styles.bigBlue}>just BigBlue</Text>
-        <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
-        <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+        <View>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
         </View>
     );
   }
