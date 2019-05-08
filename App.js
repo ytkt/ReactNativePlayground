0198d3e4d0cfbb362ad2209f360c0f13c835d7c3
ytkt <yyttkktt@gmail.com>
2019-05-08 21:57:31 +0900

    Learn about flex dimensions

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-        <View>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+        <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
         </View>
     );
   }
