a3f15e40cd9f78a73dc0a8efeb36441020e70523
ytkt <yyttkktt@gmail.com>
2019-05-08 22:20:42 +0900

    Learn about align items

--- App.js
+++ App.js
@@ -7,16 +7,12 @@ export default class App extends React.Component {
         <View style={{
           flex: 1,
           flexDirection: 'column',
-//          justifyContent: 'space-between',
-//          justifyContent: 'flex-start',
-//          justifyContent: 'center',
-//          justifyContent: 'flex-end',
-//          justifyContent: 'space-around',
-          justifyContent: 'space-evenly',
+          justifyContent: 'center',
+          alignItems: 'stretch',
         }}>
         <View style={{width: 100, height: 100, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'steelblue'}} />
+        <View style={{height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{height: 200, backgroundColor: 'steelblue'}} />
         </View>
     );
   }
